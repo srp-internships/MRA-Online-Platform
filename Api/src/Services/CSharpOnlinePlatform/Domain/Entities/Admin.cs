260bb4e040147cbb209b00364db55e913e8481bc
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-09-06 11:32:59 +0500

    implement IEntity

--- Api/src/Services/CSharpOnlinePlatform/Domain/Entities/Admin.cs
+++ Api/src/Services/CSharpOnlinePlatform/Domain/Entities/Admin.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Entities
 {
-    public class Admin : User
+    public class Admin : User, IEntity
     {
     }
 }
--- Api/src/Services/CSharpOnlinePlatform/Domain/Entities/Student.cs
+++ Api/src/Services/CSharpOnlinePlatform/Domain/Entities/Student.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Entities
 {
-    public class Student : User
+    public class Student : User, IEntity
     {
         public string Occupation { get; set; }
         public virtual ICollection<StudentCourse> Courses { get; set; } = new List<StudentCourse>();
