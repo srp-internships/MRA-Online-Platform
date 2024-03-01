953177c7ab7e19a509c983f2d19f0bbb8a0f725f
firuz2006 <firuzisoboev@gmail.com>
2024-03-01 16:10:01 +0500

    upgrade

--- Api/src/WebApi/Program.cs
+++ Api/src/WebApi/Program.cs
@@ -9,14 +9,14 @@ namespace WebApi;
 
 public class Program
 {
-    public async static Task Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var host = CreateHostBuilder(args).Build();
         
         await host.RunAsync();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
                 webBuilder.UseStartup<Startup>())
